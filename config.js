962c4ec964904a49dd218db75e3aab564fad1562
alireza116 <alireza116@gmail.com>
2021-10-12 22:21:57 -0500

    added discord key

--- config.js
+++ config.js
@@ -1,23 +1,35 @@
 module.exports = {
-    app: {
-        px: 'XXX',
-        token: 'XXX',
-        playing: 'by Zerio ❤️'
-    },
+  app: {
+    px: "_mame",
+    token: "d8aab51140af82c718ed4cc3b6f211698862bb1f78551da44767a3fa5304dc45",
+    playing: "by Toxic Femininity ❤️",
+  },
 
-    opt: {
-        DJ: {
-            enabled: false,
-            roleName: 'DJ',
-            commands: ['back', 'clear', 'filter', 'loop', 'pause', 'resume', 'seek', 'shuffle', 'skip', 'stop', 'volume']
-        },
-        maxVol: 100,
-        loopMessage: false,
-        discordPlayer: {
-            ytdlOptions: {
-                quality: 'highestaudio',
-                highWaterMark: 1 << 25
-            }
-        }
-    }
+  opt: {
+    DJ: {
+      enabled: false,
+      roleName: "DJ",
+      commands: [
+        "back",
+        "clear",
+        "filter",
+        "loop",
+        "pause",
+        "resume",
+        "seek",
+        "shuffle",
+        "skip",
+        "stop",
+        "volume",
+      ],
+    },
+    maxVol: 100,
+    loopMessage: false,
+    discordPlayer: {
+      ytdlOptions: {
+        quality: "highestaudio",
+        highWaterMark: 1 << 25,
+      },
+    },
+  },
 };
