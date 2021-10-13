3354ee48d201e66153294ed37414ee3564d5aeaf
alireza116 <alireza116@gmail.com>
2021-10-12 23:07:45 -0500

    added port

--- main.js
+++ main.js
@@ -1,21 +1,30 @@
-const { Player } = require('discord-player');
-const { Client, Intents } = require('discord.js');
+const { Player } = require("discord-player");
+const { Client, Intents } = require("discord.js");
+const express = require("express");
+
+const app = express();
+
+const port = process.env.port || 4000;
 
 global.client = new Client({
-    intents: [
-        Intents.FLAGS.GUILDS,
-        Intents.FLAGS.GUILD_MEMBERS,
-        Intents.FLAGS.GUILD_MESSAGES,
-        Intents.FLAGS.GUILD_VOICE_STATES
-    ],
-    disableMentions: 'everyone',
+  intents: [
+    Intents.FLAGS.GUILDS,
+    Intents.FLAGS.GUILD_MEMBERS,
+    Intents.FLAGS.GUILD_MESSAGES,
+    Intents.FLAGS.GUILD_VOICE_STATES,
+  ],
+  disableMentions: "everyone",
 });
 
-client.config = require('./config');
+client.config = require("./config");
 
 global.player = new Player(client, client.config.opt.discordPlayer);
 
-require('./src/loader');
-require('./src/events');
+require("./src/loader");
+require("./src/events");
 
 client.login(client.config.app.token);
+
+app.listen(port, () => {
+  console.log(`started listening to port ${port}`);
+});
