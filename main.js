f495d2fa994cb28bbe0da13eebaa656f7314db02
alireza116 <alireza116@gmail.com>
2021-10-12 23:10:42 -0500

    added port

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const express = require("express");
 
 const app = express();
 
-const port = process.env.port || 4000;
+const port = process.env.PORT || 4000;
 
 global.client = new Client({
   intents: [
